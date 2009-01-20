06db1fdb757def0ccdb7ff35f47ea0a647c43d6c
Sean Roberts <roberts.sean@gmail.com>
2009-01-20 16:01:44 -0500

    First commit

---
+++ lib/has_foreign_language.rb
@@ -0,0 +1,42 @@
+# HasForeignLanguage
+module HasForeignLanguage
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    
+    def has_foreign_language(*args)
+      args.each do |field|
+        # Define the Getter
+        define_method(field.to_s) do
+          if I18n.locale != I18n.default_locale && self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
+      	    self.send("#{field}_#{I18n.locale}".to_sym)
+      	  else
+      	    super
+      	  end
+      	end
+      	
+      	# Define the Setter
+      	define_method("#{field}=") do |val|
+      	  if I18n.locale != I18n.default_locale && self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
+      	    self["#{field}_#{I18n.locale}".to_sym] = val
+      	  else
+      	    self[field.to_sym] = val
+      	  end
+      	end
+      	
+      	# Define the Default Getter
+      	define_method("#{field}_#{I18n.default_locale}") do
+          self.send(field.to_sym)
+      	end
+      	
+      	# Define the Default Setter
+      	define_method("#{field}_#{I18n.default_locale}=") do |val|
+      	  self[field.to_sym] = val
+      	end
+      end
+    end
+  end
+end
+      
