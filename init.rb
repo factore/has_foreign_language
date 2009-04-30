f17f05bc77ccea60a4078015d1f3d49f55e67a3d
Sean Roberts <roberts.sean@gmail.com>
2009-04-30 09:18:56 -0400

    Fix to deal with the new InstanceTag thing in Rails 2.3

--- init.rb
+++ init.rb
@@ -1,2 +1,5 @@
 # Include hook code here
-ActiveRecord::Base.send(:include, HasForeignLanguage)
+require 'has_foreign_language'
+require 'form_fix'
+ActiveRecord::Base.send(:include, Factore::HasForeignLanguage)
+#ActionView::Helpers::FormHelper.send(:extend, Factore::FormFix)
--- lib/has_foreign_language.rb
+++ lib/has_foreign_language.rb
@@ -1,43 +1,44 @@
 # HasForeignLanguage
-module HasForeignLanguage
-  def self.included(mod)
-    mod.extend(ClassMethods)
-  end
+module Factore
+  module HasForeignLanguage
+    def self.included(mod)
+      mod.extend(ClassMethods)
+    end
   
-  module ClassMethods
+    module ClassMethods
     
-    def has_foreign_language(*args)
-      args.each do |field|
-        # Define the Getter
-        define_method(field.to_s) do
+      def has_foreign_language(*args)
+        args.each do |field|
+          # Define the Getter
+          define_method(field.to_s) do
+            if I18n.locale != I18n.default_locale && self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
+              result=self.send("#{field}_#{I18n.locale}".to_sym)
+              result.blank? ? super : self.send("#{field}_#{I18n.locale}".to_sym) #falling back to default if requested locale is nil
+            else
+              super
+            end
+          end
+
+        # Define the Setter
+        define_method("#{field}=") do |val|
           if I18n.locale != I18n.default_locale && self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
-            result=self.send("#{field}_#{I18n.locale}".to_sym)
-            result.blank? ? super : self.send("#{field}_#{I18n.locale}".to_sym) #falling back to default if requested locale is nil
+            self["#{field}_#{I18n.locale}".to_sym] = val
           else
-            super
+            self[field.to_sym] = val
           end
         end
 
-      # Define the Setter
-      define_method("#{field}=") do |val|
-        if I18n.locale != I18n.default_locale && self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
-          self["#{field}_#{I18n.locale}".to_sym] = val
-        else
+        # Define the Default Getter
+        define_method("#{field}_#{I18n.default_locale}") do
+          self.send(field.to_sym)
+        end
+
+        # Define the Default Setter
+        define_method("#{field}_#{I18n.default_locale}=") do |val|
           self[field.to_sym] = val
         end
-      end
-
-      # Define the Default Getter
-      define_method("#{field}_#{I18n.default_locale}") do
-        self.send(field.to_sym)
-      end
-
-      # Define the Default Setter
-      define_method("#{field}_#{I18n.default_locale}=") do |val|
-        self[field.to_sym] = val
-      end
+        end
       end
     end
   end
-end
-      
+end 
