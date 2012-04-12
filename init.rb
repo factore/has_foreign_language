25fa667533a225840b0831d518d42f96473e82e4
Sean <roberts.sean@gmail.com>
2012-04-12 15:22:14 -0400

    Fix the readme for Github

--- init.rb
+++
@@ -1,5 +0,0 @@
-# Include hook code here
-require 'has_foreign_language'
-require 'form_fix'
-ActiveRecord::Base.send(:include, Factore::HasForeignLanguage)
-#ActionView::Helpers::FormHelper.send(:extend, Factore::FormFix)
